30c09e786e66ca6b45d7d65a07bc1679a025748f
volde_morticia <107201401+volde-morticia@users.noreply.github.com>
2022-07-14 07:19:36 +0000

    added a line to extend the snake's length every time it eats

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ while game_is_on:
   if snake.head.distance(food) < 15:
     food.refresh()
     scoreboard.score_add()
+    snake.extend()
 
   if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
     game_is_on = False
